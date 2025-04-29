5b00ef460dcd20b3f6cd6c0effb638c1cad33fce
Ignacio Talvi <ignaciotalvi@Ignacios-MacBook-Pro.local>
2025-04-29 16:01:28 +0200

    add/changes server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-
+const request = require("request");
+const apiKey = "*****************";
 const bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
@@ -11,6 +12,32 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", function (req, res) {
   // NEW CODE
   res.render("index");
+});
+
+app.post("/", function (req, res) {
+  res.render("index");
+  console.log(req.body.city);
+});
+
+app.post("/", function (req, res) {
+  let city = req.body.city;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+  request(url, function (err, response, body) {
+    if (err) {
+      res.render("index", { weather: null, error: "Error, please try again" });
+    } else {
+      let weather = JSON.parse(body);
+      if (weather.main == undefined) {
+        res.render("index", {
+          weather: null,
+          error: "Error, please try again",
+        });
+      } else {
+        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+        res.render("index", { weather: weatherText, error: null });
+      }
+    }
+  });
 });
 
 app.listen(3000, function () {
